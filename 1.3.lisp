66f521e5f4a77204ab7e4640969f3d1589ac04b3
Kirill "Nemoden" K <i@nemoden.com>
2013-01-27 00:36:51 +1100

    1.2 and 1.3

---
+++ 1.3.lisp
@@ -0,0 +1,7 @@
+(define (square x) (* x x))
+(define (sum-of-squares x y) (+ (square x) (square y)))
+(define (min x y) (if (< x y) x y))
+(define (max x y) (if (> x y) x y))
+(define (sum-of-squares-of-2-max x y z)
+  (sum-of-squares (max x y) (max z (min x y)))
+)
